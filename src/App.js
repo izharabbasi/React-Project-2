42d855e9715d630d15fe3f731e35b9af30b0cf91
izharabbasi <izharabbasi103@gmail.com>
2020-09-25 01:04:54 +0500

    apply daytime

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import './App.css';
 
 function App() {
   let [count, setcount] = useState(0);
-  let [isMorning, setMorning] = useState(false);
-
-  return (
-    <div className={`box ${isMorning ? 'time': ''}`}>
-      <h3>GoodNight {isMorning ? 'Morning': 'Night'}</h3>
-      <Message  counter={count}/>
-      <button onClick={()=> setcount(++count)}>Next Count</button>
-      <button onClick={()=> setMorning(!isMorning)}>Update</button>
-      
+  let [isMorning, setMoring] = useState(true);
+  return(
+    <div className={`box ${isMorning ? "time" : ''}`}>
+      <h3 >Have A Good {isMorning ? 'Morning' : 'Night'}</h3>
+      <Message counter={count}/>
+      <button onClick={()=>setcount(++count)}>counter</button>
+      <button onClick={()=> setMoring(!isMorning)}>Update DayTime</button>
     </div>
   );
+  
 }
 
 export default App;
