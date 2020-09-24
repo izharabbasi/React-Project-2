f939c11283bed9c365e5673a36acaae8196b4b70
izharabbasi <izharabbasi103@gmail.com>
2020-09-25 00:53:26 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React , {useState}from 'react';
+import Message from './message'
 import './App.css';
 
 function App() {
-  let [count, setcount] = useState(0)
+  let [count, setcount] = useState(0);
+  let [isMorning, setMorning] = useState(false);
 
   return (
-    <div>
-      <h3>The value of counter is: {count}</h3>
+    <div className={`box ${isMorning ? 'time': ''}`}>
+      <h3>GoodNight {isMorning ? 'Morning': 'Night'}</h3>
+      <Message  counter={count}/>
       <button onClick={()=> setcount(++count)}>Next Count</button>
+      <button onClick={()=> setMorning(!isMorning)}>Update</button>
       
     </div>
   );
---
+++ src/message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Message(props){
+    return(
+        <h3>The value of counter is: {props.counter}</h3>
+    );
+}
+
+export default Message;
